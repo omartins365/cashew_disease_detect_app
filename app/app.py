72dd3b92044c2a74adec234e9a60e5b7ada52039
Martins Ayoola <omartins365@gmail.com>
2025-06-29 07:10:55 +0100

    Add application logo to the sidebar for enhanced branding

--- app/app.py
+++ app/app.py
@@ -277,7 +277,8 @@ def main():
     # st.sidebar.title("🌰 Cashew Disease Detection")
     st.sidebar.markdown("Upload an image or video of cashew leaves, nuts or stem to detect diseases using the custom YOLOv10 model.")
 
-
+    st.logo("app/assets/logo.png", width=200)
+    
     
     model = select_model()
     
